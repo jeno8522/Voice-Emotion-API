06154a91cf2695e5b873b7cd4e5c67f186f8336f
pantokrator <pantokrator3841@gmail.com>
2022-05-26 23:45:23 +0900

    s

--- emotion_detection.py
+++ emotion_detection.py
@@ -108,7 +108,7 @@ def getEmotion(filename):
 
     scaler = joblib.load("scaler.joblib")
     le = joblib.load("labelEncoder.joblib")
-    model = torch.load('model_0516.pt', map_location='cpu')
+    model = torch.load('model_0526.pt', map_location='cpu')
 
     test_data = loadAudioFiles(filename)
 
